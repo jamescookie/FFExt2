da5a7c0f851ef2f55e4ca7a2c196af4cbff9fd09
James <James@be3fd238-c909-4725-b312-79b88f520c4f>
2006-12-28 14:14:29 +0000

    Version 0.5: Remove Javascript warnings & add help button
    git-svn-id: http://192.168.1.66/svn/FFext/trunk/ffext2@34 be3fd238-c909-4725-b312-79b88f520c4f

--- datepicker/chrome/content/datepicker/preferenceUtils.js
+++ datepicker/chrome/content/datepicker/preferenceUtils.js
@@ -11,12 +11,16 @@ function getBooleanPreferenceValue(fieldName, prefs) {
 }
 
 function getCharacterPreferenceValue(fieldName, prefs) {
+	return getCharacterPreferenceValueWithDefault(fieldName, prefs, "Unknown");
+}
+
+function getCharacterPreferenceValueWithDefault(fieldName, prefs, defaultValue) {
 	var tmp;
 
 	if (prefs.getPrefType("@EXTENSION@."+fieldName) == prefs.PREF_STRING){
 		tmp = prefs.getCharPref("@EXTENSION@."+fieldName);
 	} else {
-		tmp = "Unknown";
+		tmp = defaultValue;
 	}
 
 	return tmp;
