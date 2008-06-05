9e9e3f05169b524335886ce4192d68de8ce258c7
James <James@be3fd238-c909-4725-b312-79b88f520c4f>
2008-06-05 21:25:36 +0000

    Version 2: First stab - change to use XUL calendar
    git-svn-id: http://192.168.1.66/svn/FFext/trunk/ffext2@43 be3fd238-c909-4725-b312-79b88f520c4f

--- datepicker/chrome/content/datepicker/showCal.js
+++ datepicker/chrome/content/datepicker/showCal.js
@@ -1,13 +1,13 @@
 function init() {
-    @EXTENSION@LanguageVar.loadLanguage();
-    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-    var cal = new Calendar(getIntegerPreferenceValue("weekStart", prefs, 0),
-            getIntegerPreferenceValue("weekend1", prefs, 0) + "," + getIntegerPreferenceValue("weekend2", prefs, 6));
-    cal.showsOtherMonths = getBooleanPreferenceValue("showOtherMonths", prefs);
-    cal.setRange(getIntegerPreferenceValue("years1", prefs, 1900), getIntegerPreferenceValue("years2", prefs, 2199));
-    cal.setDateFormat(getCharacterPreferenceValue("dateFormat", prefs, "%d %B %Y"));
-    cal.create(document.getElementById("calendarDiv"));
-    cal.callHandler();
+//    @EXTENSION@LanguageVar.loadLanguage();
+//    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+//    var cal = new Calendar(getIntegerPreferenceValue("weekStart", prefs, 0),
+//            getIntegerPreferenceValue("weekend1", prefs, 0) + "," + getIntegerPreferenceValue("weekend2", prefs, 6));
+//    cal.showsOtherMonths = getBooleanPreferenceValue("showOtherMonths", prefs);
+//    cal.setRange(getIntegerPreferenceValue("years1", prefs, 1900), getIntegerPreferenceValue("years2", prefs, 2199));
+//    cal.setDateFormat(getCharacterPreferenceValue("dateFormat", prefs, "%d %B %Y"));
+//    cal.create(document.getElementById("calendarDiv"));
+//    cal.callHandler();
 }
 
 function getIntegerPreferenceValue(fieldName, prefs, defaultValue) {
