c6d264306725e3393393a1e378f16861032d2f34
James <James@be3fd238-c909-4725-b312-79b88f520c4f>
2008-06-24 21:33:44 +0000

    Version 2: First stab - change to use XUL calendar
    git-svn-id: http://192.168.1.66/svn/FFext/trunk/ffext2@44 be3fd238-c909-4725-b312-79b88f520c4f

--- datepicker/chrome/content/datepicker/showCal.js
+++ datepicker/chrome/content/datepicker/showCal.js
@@ -1,6 +1,8 @@
+var @EXTENSION@DateFormat
+
 function init() {
-//    @EXTENSION@LanguageVar.loadLanguage();
-//    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+    @EXTENSION@LanguageVar.loadLanguage();
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 //    var cal = new Calendar(getIntegerPreferenceValue("weekStart", prefs, 0),
 //            getIntegerPreferenceValue("weekend1", prefs, 0) + "," + getIntegerPreferenceValue("weekend2", prefs, 6));
 //    cal.showsOtherMonths = getBooleanPreferenceValue("showOtherMonths", prefs);
@@ -8,6 +10,11 @@ function init() {
 //    cal.setDateFormat(getCharacterPreferenceValue("dateFormat", prefs, "%d %B %Y"));
 //    cal.create(document.getElementById("calendarDiv"));
 //    cal.callHandler();
+    this.@EXTENSION@DateFormat = getCharacterPreferenceValue("dateFormat", prefs, "%d %B %Y");
+}
+
+function showDate(aDate) {
+    document.getElementById("datebox").value = aDate.print(@EXTENSION@DateFormat);
 }
 
 function getIntegerPreferenceValue(fieldName, prefs, defaultValue) {
