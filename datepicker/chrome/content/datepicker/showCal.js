e951391780c328789a868f497cd88ab9adc009b9
james <james@be3fd238-c909-4725-b312-79b88f520c4f>
2009-03-09 23:21:09 +0000

    2.1.51: Add 'today' button. Change 'ok' to 'close'
    git-svn-id: http://192.168.1.66/svn/FFext/trunk/ffext2@279 be3fd238-c909-4725-b312-79b88f520c4f

--- datepicker/chrome/content/datepicker/showCal.js
+++ datepicker/chrome/content/datepicker/showCal.js
@@ -1,16 +1,29 @@
 var @EXTENSION@ShowVar = {
     @EXTENSION@DateFormat: null,
+    firstdayofweek: 0,
 
     init: function() {
         @EXTENSION@LanguageVar.loadLanguage();
         var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
         this.@EXTENSION@DateFormat = @EXTENSION@PrefUtilsVar.getCharacterPreferenceValue("dateFormat", prefs, "%d %B %Y");
-        var cal = document.createElement("datepicker")
+        this.firstdayofweek = @EXTENSION@PrefUtilsVar.getIntegerPreferenceValue("weekStart", prefs, 0);
+        this.createNewCalendar();
+    },
+
+    createNewCalendar: function() {
+        var cal = document.createElement("datepicker");
         cal.setAttribute('type', 'grid');
         cal.setAttribute('onchange', '@EXTENSION@ShowVar.showDate(this.dateValue)');
-        cal.setAttribute('firstdayofweek', @EXTENSION@PrefUtilsVar.getIntegerPreferenceValue("weekStart", prefs, 0));
-        document.getElementById("datepickercontainer").appendChild(cal)
+        cal.setAttribute('firstdayofweek', this.firstdayofweek);
+        cal.setAttribute('id', 'datepicker');
+        document.getElementById("datepickercontainer").appendChild(cal);
         this.showDate(new Date());
+    },
+
+    today: function() {
+        var cal = document.getElementById("datepicker");
+        document.getElementById("datepickercontainer").removeChild(cal);
+        this.createNewCalendar();
     },
 
     showDate: function(aDate) {
